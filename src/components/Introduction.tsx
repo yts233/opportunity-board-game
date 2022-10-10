f9809fdc37fb08e82e48be791e4acec8eb5d2934
Ye_Tianshun <admin@yts233.tk>
2022-10-10 10:32:35 +0800

    backup

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,6 @@
 export default function Introduction() {
-    return <>机遇棋是由up主的同学设计的一种棋盘类游戏。游戏大致规则是：
+    return <>机遇棋是由<a href={'https://space.bilibili.com/413300639'}>up主</a>的<a
+        href={'https://space.bilibili.com/1540652520'}>同学</a>设计的一种棋盘类游戏。游戏大致规则是：
         <br/>
         步兵克制长矛手，长矛手克制骑士，骑士克制步兵
         <br/>
@@ -8,5 +9,9 @@ export default function Introduction() {
         纵火者可以与其他种类的棋相互残杀，除国王外所有棋子都可与纵火者相互残杀
         <br/>
         巫师可以与任意我方棋子互换，但只能使用一次
+        <br/>
+        <a href={'https://www.bilibili.com/video/BV1qe4y1z7fp'}>同学的视频</a>
+        <br/>
+        <a href={'https://github.com/yts233/opportunity-board-game/'}>github</a>
     </>;
 }
